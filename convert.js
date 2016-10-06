fb9e2acadf19d018e59d6b5c33693e7576608eab
kent <utterback.kent.a@gmail.com>
2016-10-06 18:00:22 -0400

    Finished, maybe?

--- convert.js
+++ convert.js
@@ -16,20 +16,20 @@ var convert = {
 
   unix: 0,
 
-  moty: {
-    noLeap: [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365],
-    leap:   [31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366],
-    month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    monthindex: 0
-  },
+  noLeap: [1, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334],
+
+  leap:   [1, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335],
+
+  montharr: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+
+  monthindex: 0,
 
   isleap: false,
 
   unixtonatural: function(unix){
     this.unix = unix;
     this.year = Math.floor((this.unix / 31557600) + 1970);
-    this.day = Math.ceil((this.unix % 31557600) / 86400);
-    console.log(Math.ceil((this.unix % 31557600)/86400));
+    this.day = Math.round((this.unix % 31557600)/86400) + 1;
     if(this.year % 4 == 0){
       this.isleap = true;
     }
@@ -39,21 +39,21 @@ var convert = {
 
     if(this.isleap == true){
       for(var i = 0; i <= 11; i++){
-        if(this.day <= this.moty.leap[i]){
-          this.moty.monthindex = i;
+        if(this.leap[i] <= this.day){
+          this.monthindex = i;
         }
       }
-      this.month = this.moty.month[this.moty.monthindex];
-      this.dom = this.day - this.moty.leap[this.moty.monthindex] + 1;
+      this.month = this.montharr[this.monthindex];
+      this.dom = (this.day - this.leap[this.monthindex]) + 1;
     }
     else{
-      for(var i = 0; i <= 12; i++){
-        if(this.day <= this.moty.noleap[i]){
-          this.moty.monthindex = i;
+      for(var i = 0; i <= 11; i++){
+        if(this.noLeap[i] <= this.day){
+          this.monthindex = i;
         }
       }
-      this.month = this.moty.month[this.moty.monthindex];
-      this.dom = this.day - this.moty.noleap[this.moty.monthindex] + 1;
+      this.month = this.montharr[this.monthindex];
+      this.dom = (this.day - this.noLeap[this.monthindex]) + 1;
     };
 
     this.natural = this.month + " " + this.dom + ", " + this.year;
@@ -62,7 +62,7 @@ var convert = {
 
   naturaltounix: function(natural){
     this.natural = natural;
-    this.unix = Date.parse(this.natural)/100000;
+    this.unix = Date.parse(this.natural)/1000;
     console.log("convert.naturaltounix: " + this.unix + " : " + this.natural);
     return this.unix;
   }
